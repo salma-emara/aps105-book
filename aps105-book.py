093c9235824d7cd3e0aa6f7a58d475cc4b361102
Salma Emara <salma.emara@mail.utoronto.ca>
2022-10-14 18:07:54 -0400

    Added docs setup

---
+++ aps105-book.py
@@ -0,0 +1,5 @@
+"""
+This is where I start writing an online book for APS105.
+"""
+
+__version__ = "0.1.0"
---
+++ docs/source/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'APS 105: Computer Fundamentals Online Book'
+copyright = '2022, Salma Emara'
+author = 'Salma Emara'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "sphinx.ext.duration",
+    "sphinx.ext.autosectionlabel",
+    "jupyter_sphinx",
+    "sphinx_thebe",
+    "nbsphinx",
+    ]
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']

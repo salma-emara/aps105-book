ee74ef72ccb808c2cafa8a5b2cb6be5f8dbe41db
Salma Emara <salma.emara@mail.utoronto.ca>
2022-12-23 04:49:25 -0500

    Added code of chapter 4

---
+++ textbook/code/chapter04/sum-numbers-while/sum-numbers-while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  int sum = 0;
+  int num;
+  printf("Enter a number: ");
+  scanf("%d", &num);
+  while (num >= 0) {
+    sum += num;
+    printf("Enter another number: ");
+    scanf("%d", &num);
+  }
+  printf("The sum is %d\n", sum);
+  return 0;
+}

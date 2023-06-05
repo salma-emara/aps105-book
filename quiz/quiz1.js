c40ace47eff6c82559ae3e46d729565883bafa50
Riya Bansal <riyabansal0413@gmail.com>
2023-06-05 22:40:26 +0530

    temp file for testing

---
+++ quiz/quiz1.js
@@ -0,0 +1,12 @@
+let parsedObject;
+
+parsedObject = {
+    "question": "sample question 1",
+    "choices": [
+        "A",
+        "B",
+        "C"
+    ],
+    "answer": "C",
+    "hint": "some explanation"
+}

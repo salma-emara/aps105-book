5afbd04aa9fe87f03cdd2273dc3b22e2d69c0ae9
Riya Bansal <riyabansal0413@gmail.com>
2023-06-05 23:23:30 +0530

    implementing the .js quiz files.

---
+++ quiz/quiz2.js
@@ -0,0 +1,12 @@
+let parsedObject;
+
+parsedObject = {
+    "question": "sample question 2",
+    "choices": [
+        "Op1",
+        "Op2",
+        "Op3"
+    ],
+    "answer": "Op2",
+    "hint": "some more explanation"
+}

28822654c85d81d22e7ab883a652ffba63d295c3
Riya Bansal <riyabansal0413@gmail.com>
2023-06-14 23:22:56 +0530

    added toml to js convertor file

--- quiz/quiz1.js
+++ quiz/quiz1.js
@@ -10,3 +10,24 @@ parsedObject = {
     "answer": "C",
     "hint": "some explanation"
 }
+
+
+
+
+{
+    questions: [
+      {
+        question: 'sample question 1',
+        choices: ['A', 'B', 'C'],
+        answer: 'C',
+        hint: 'some explanation'
+      },
+      {
+        question: 'sample question new',
+        choices: ['A1', 'B1', 'C1'],
+        answer: 'C1',
+        hint: 'some explanation for answer'
+      }
+    ]
+  }
+  

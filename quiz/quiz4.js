097d0b46ecad9cfc3196ea51e28eef6589b9c69e
Riya Bansal <riyabansal0413@gmail.com>
2023-06-26 23:45:14 +0530

    completed formatting for quizzes + edited code

---
+++ quiz/quiz4.js
@@ -0,0 +1,43 @@
+let parsedObject; 
+  parsedObject = {
+  "questions": [
+    {
+      "question": "sample question 1",
+      "codeSnippet": "printf(1+2);\nprintf(\"Hello World!\");",
+      "choices": [
+        "A",
+        "B",
+        "C"
+      ],
+      "answer": [
+        "C"
+      ],
+      "hint": "some explanation"
+    },
+    {
+      "question": "sample question new",
+      "choices": [
+        "A1",
+        "B1",
+        "C1"
+      ],
+      "answer": [
+        "C1",
+        "A1"
+      ],
+      "hint": "some explanation for answer"
+    },
+    {
+      "question": "sample question 3",
+      "choices": [
+        "A1",
+        "B1",
+        "three"
+      ],
+      "answer": [
+        "three"
+      ],
+      "hint": "some explanation for answer"
+    }
+  ]
+};
--- textbook/_static/app.js
+++ textbook/_static/app.js
@@ -1,17 +1,24 @@
 let currentQuestionIndex = 0;
 
 function startQuiz() {
+
     currentQuestionIndex = 0;
+    console.log(currentQuestionIndex);
 
     const quizForms = document.querySelectorAll("[id^='quizForm']");
+
+    for (let i = 1; i < quizForms.length; i++) {
+        quizForms[i].style.display = "none";
+    }
+
     const currentForm = quizForms[currentQuestionIndex];
     currentForm.style.display = "block";
 
     var fullscreenForm = document.getElementById("fullscreen-form");
     fullscreenForm.classList.add("active");
 
-    const nextButton = document.getElementById("next-button");
-    nextButton.classList.remove("hidden");
+    const containerheader = document.getElementById("container-header");
+    containerheader.classList.add("hidden");
 
     if (quizForms.length === 1) {
         nextButton.classList.add("hidden");
@@ -19,51 +26,134 @@ function startQuiz() {
 }
 
 function closeFullscreenForm() {
-    var fullscreenForm = document.getElementById("fullscreen-form");
-    fullscreenForm.classList.remove("active");
-    fullscreenForm.classList.add("mini");
 
-    // Show all questions
-    const quizForms = document.querySelectorAll("[id^='quizForm']");
-    for (let i = 0; i < quizForms.length; i++) {
-        quizForms[i].style.display = "block";
-        quizForms[i].style.border = "none";
+    var forms = document.querySelectorAll("[id^='quizForm']");
+    var lastForm = forms[forms.length - 1];
+    var messageElement = lastForm.querySelector("p[id^='message']");
+
+    if (messageElement && (messageElement.textContent !== "" && messageElement.textContent !== "Please make a selection.")) {
+        var fullscreenForm = document.getElementById("fullscreen-form");
+
+        fullscreenForm.classList.remove("active");
+        fullscreenForm.classList.add("mini");
+
+        const containerheader = document.getElementById("container-header");
+        containerheader.classList.add("hidden");
+
+        // Show all questions
+        const quizForms = document.querySelectorAll("[id^='quizForm']");
+        for (let i = 0; i < quizForms.length; i++) {
+            quizForms[i].style.display = "block";
+            quizForms[i].style.border = "none";
+
+            if (i !== quizForms.length - 1) {
+                var hrElement = document.createElement('hr');
+                hrElement.style.borderColor = "darkgrey";
+                quizForms[i].appendChild(hrElement);
+            }
+        }
+
+        var startButton = document.getElementById("start-button");
+        startButton.classList.add("hidden");
+
+        var closeButton = document.getElementById("close-button");
+        closeButton.classList.add("hidden");
+
+        var submitButtons = document.getElementsByClassName("submit-button");
+
+        for (var i = 0; i < submitButtons.length; i++) {
+            var submitButton = submitButtons[i];
+            submitButton.classList.add("hidden");
+        }
+
+        var quizTitles = document.getElementsByClassName("quiz-title");
+
+        for (var i = 0; i < quizTitles.length; i++) {
+            var quizTitle = quizTitles[i];
+            quizTitle.classList.add("hidden");
+        }
+
+        var nextButtons = document.getElementsByClassName("next-button");
+
+        for (var i = 0; i < nextButtons.length; i++) {
+            var nextButton = nextButtons[i];
+            nextButton.classList.add("hidden");
+        }
     }
+    else {
+        var fullscreenForms = document.getElementById("fullscreen-form");
 
-    var closeButton = document.getElementById("close-button");
-    closeButton.classList.add("hidden");
-
-    var startButton = document.getElementById("start-button");
-    startButton.classList.add("hidden");
-
-    var nextButton = document.getElementById("next-button");
-    nextButton.classList.add("hidden");
+        fullscreenForms.classList.remove("active");
+        fullscreenForms.classList.remove("mini");
+        fullscreenForms.classList.add("hidden");
+        const containerheader = document.getElementById("container-header");
+        containerheader.classList.remove("hidden");
+    }
 }
-
 
 function parse_and_generate_form(fileName) {
 
+    const doc = document.getElementById("fullscreen-form");
+
     const questions = parsedObject.questions;
-    //const quizContainer = document.getElementById("quiz-container");
+
+    const questionNumberElement = document.getElementById("question-number");
+    questionNumberElement.innerHTML = questions.length + " Questions";
+
+
 
     for (let i = 0; i < questions.length; i++) {
-        const question = questions[i].question;
-        const choices = questions[i].choices;
+        const question = questions[i].prompt;
+        const choices = questions[i].distractors;
         const codeSnippet = questions[i].codeSnippet;
         const answer = questions[i].answer;
-        const hint = questions[i].hint;
+        const hint = questions[i].explainations;
 
         //generate the quiz form in HTML
         const form = document.createElement("form");
         form.id = "quizForm" + (i + 1);
         form.classList.add("quiz-form");
 
+        // Create the container div
+        const container = document.createElement("div");
+        container.classList.add("header");
+
+        // Create the quiz title
+        const title = document.createElement("h3");
+        title.innerHTML = "Quiz";
+        title.classList.add("quiz-title");
+        title.id = "quiz-title";
+        title.style.fontWeight = "bold";
+
+        // Create the counter
+        const counter = document.createElement("p");
+        counter.innerHTML = "Question " + (i + 1) + " / " + questions.length;
+        counter.style.textAlign = "right";
+
+        // Append the title and counter to the container div
+        container.appendChild(title);
+        container.appendChild(counter);
+
+        // Append the container div to the form
+        form.appendChild(container);
+
+
         //add question text
-        const questionElement = document.createElement("p");
+        const questionElement = document.createElement("div");
         questionElement.id = "question" + (i + 1);
-        questionElement.innerHTML = question;
         questionElement.classList.add("question-box");
+
+        const questionTitle = document.createElement("h4");
+        questionTitle.innerHTML = "Question " + (i + 1);
+
+        const questionText = document.createElement("p");
+        questionText.innerHTML = question;
+
+        questionElement.appendChild(questionTitle);
+        questionElement.appendChild(questionText);
+
         form.appendChild(questionElement);
+
 
         // Add code snippet if available
         if (codeSnippet) {
@@ -71,12 +161,10 @@ function parse_and_generate_form(fileName) {
             codeSnippetContainer.classList.add("code-snippet");
 
             const codeSnippetLinesContainer = document.createElement("div");
-            codeSnippetLinesContainer.classList.add("code-lines");
 
             const codeSnippetLines = codeSnippet.trim().split("\n");
             for (let j = 0; j < codeSnippetLines.length; j++) {
                 const codeLineContainer = document.createElement("div");
-                codeLineContainer.classList.add("code-line-container");
 
                 const lineNumberElement = document.createElement("span");
                 lineNumberElement.classList.add("line-number");
@@ -84,7 +172,6 @@ function parse_and_generate_form(fileName) {
                 codeLineContainer.appendChild(lineNumberElement);
 
                 const codeLineElement = document.createElement("span");
-                codeLineElement.classList.add("code-line");
                 codeLineElement.textContent = codeSnippetLines[j];
                 codeLineContainer.appendChild(codeLineElement);
 
@@ -110,48 +197,53 @@ function parse_and_generate_form(fileName) {
         });
         form.appendChild(submitButton);
 
-        // Add next button
-        const nextButton = document.createElement("button");
-        nextButton.type = "button";
-        nextButton.innerHTML = "Next";
-        nextButton.classList.add("next-button");
-        nextButton.addEventListener("click", showNextQuestion);
-        form.appendChild(nextButton);
-
         //add text for after submission
         const messageElement = document.createElement("p");
         messageElement.id = "message" + (i + 1);
         form.appendChild(messageElement);
 
+        // Add next button
+        const nextButton = document.createElement("button");
+        nextButton.type = "button";
+        nextButton.id = "next-button";
+        nextButton.innerHTML = "Next";
+        nextButton.classList.add("next-button");
+        nextButton.classList.add("hidden");
+        nextButton.addEventListener("click", showNextQuestion);
+        form.appendChild(nextButton);
+
         document.getElementById("fullscreen-form").appendChild(form);
 
-        console.log(answer.length);
         if (answer.length === 1) {
             // Create a radio button choices
             for (let j = 0; j < choices.length; j++) {
-
                 const choice = choices[j];
 
-                //create radio buttons
+                // Create a container div
+                const choiceContainer = document.createElement("div");
+                choiceContainer.classList.add("label");
+
+                // Create the radio button
                 const radioButton = document.createElement("input");
                 radioButton.type = "radio";
                 radioButton.name = "choice" + (i + 1);
                 radioButton.value = choice;
                 radioButton.id = "choice" + (i + 1) + "-" + (j + 1);
 
-                //add labels
+                // Create the label
                 const label = document.createElement("label");
-                label.textContent = choice;
+                label.innerHTML = choice.replace(/\n/g, "<br>");
                 label.setAttribute("for", "choice" + (i + 1) + "-" + (j + 1));
-                const space = document.createElement("span");
-                space.innerHTML = "&nbsp;";
 
-                //append the buttons to Choices element
-                choicesElement.appendChild(radioButton);
-                choicesElement.appendChild(space);
-                choicesElement.appendChild(label);
+                // Append the radio button and label to the container div
+                choiceContainer.appendChild(radioButton);
+                choiceContainer.appendChild(label);
+
+                // Append the container div to the choices element
+                choicesElement.appendChild(choiceContainer);
                 choicesElement.appendChild(document.createElement("br"));
             }
+
         }
 
         else {
@@ -159,35 +251,36 @@ function parse_and_generate_form(fileName) {
             for (let j = 0; j < choices.length; j++) {
                 const choice = choices[j];
 
-                // Create checkboxes
+                // Create a container div
+                const choiceContainer = document.createElement("div");
+                choiceContainer.classList.add("label");
+
+                // Create the checkbox
                 const checkbox = document.createElement("input");
                 checkbox.type = "checkbox";
                 checkbox.name = "choice" + (i + 1);
                 checkbox.value = choice;
                 checkbox.id = "choice" + (i + 1) + "-" + (j + 1);
 
-                // Add labels
+                // Create the label
                 const label = document.createElement("label");
-                label.textContent = choice;
+                label.innerHTML = choice.replace(/\n/g, "<br>");
                 label.setAttribute("for", "choice" + (i + 1) + "-" + (j + 1));
-                const space = document.createElement("span");
-                space.innerHTML = "&nbsp;";
 
-                // Append the checkboxes to the choices element
-                choicesElement.appendChild(checkbox);
-                choicesElement.appendChild(space);
-                choicesElement.appendChild(label);
+                // Append the checkbox and label to the container div
+                choiceContainer.appendChild(checkbox);
+                choiceContainer.appendChild(label);
+
+                // Append the container div to the choices element
+                choicesElement.appendChild(choiceContainer);
                 choicesElement.appendChild(document.createElement("br"));
             }
-
         }
-
 
         if (i > 0) {
             form.style.display = "none";
         }
     }
-
 }
 
 
@@ -198,34 +291,27 @@ function handle_submission(formId, answer, hint) {
     const messageElement = form.querySelector("#message" + formId.slice(8));
 
     if (selectedChoices.length > 0) {
-        // Convert selected choices to an array of their values
-        const selectedValues = Array.from(selectedChoices).map(choice => choice.value);
+        const selectedIndices = Array.from(selectedChoices).map(choice => parseInt(choice.id.split('-')[1] - 1, 10));
+        const isCorrect = answer.some(correctIndex => selectedIndices.includes(correctIndex));
+        const explainIndex = selectedIndices[0];
 
-        if (Array.isArray(answer)) {
-            // Check if all selected values are present in the answer array
-            const isCorrect = selectedValues.every(value => answer.includes(value));
-
-            if (isCorrect) {
-                messageElement.innerHTML = "Correct answer!";
-                messageElement.style.color = "green";
-            } else {
-                messageElement.innerHTML = "Wrong answer" + hint;
-                messageElement.style.color = "red";
-            }
+        if (isCorrect) {
+            messageElement.innerHTML = "Correct! <span class='hint-text'>" + hint[explainIndex].replace(/\n/g, "<br>"); + "</span>";
+            messageElement.style.color = "green";
+            messageElement.style.fontWeight = "bold";
         } else {
-            // Handle case when answer is a single value
-            if (selectedValues.includes(answer)) {
-                messageElement.innerHTML = "Correct answer!";
-                messageElement.style.color = "green";
-            } else {
-                messageElement.innerHTML = "Wrong answer" + hint;
-                messageElement.style.color = "red";
-            }
+            messageElement.innerHTML = "Inorrect! <span class='hint-text'>" + hint[explainIndex].replace(/\n/g, "<br>"); + "</span>";
+            messageElement.style.color = "red";
+            messageElement.style.fontWeight = "bold";
         }
     } else {
-        messageElement.innerHTML = "Please select";
+        messageElement.innerHTML = "Please make a selection.";
         messageElement.style.color = "red";
+        messageElement.style.fontWeight = "bold";
     }
+
+    const nextButton = document.getElementById("next-button");
+    nextButton.classList.remove("hidden");
 }
 
 
@@ -250,5 +336,3 @@ function showNextQuestion() {
         nextButton.classList.remove("hidden");
     }
 }
-
-

37e0107d30904694d9ef6983892ce2b6a8c570a1
Yvonne Yang <yvonne.y.yang@outlook.com>
2023-07-27 01:18:26 -0400

    indent

--- textbook/_static/code-runner-setup.js
+++ textbook/_static/code-runner-setup.js
@@ -1,1 +1,11 @@
-let CodeRunner_LightOrDarkMode = 'dark';
+/* CodeRunner_LightOrDarkMode
+    values: "light" or "dark"
+    default: "light"
+*/
+let CodeRunner_LightOrDarkMode = 'light';
+
+/* CodeRunner_AceEditor_Theme
+  values: https://github.com/ajaxorg/ace/tree/master/src/theme
+  default: "tomorrow" if CodeRunner_LightOrDarkMode = "light", and "monokai" if CodeRunner_LightOrDarkMode = "dark"
+*/
+// let CodeRunner_AceEditor_Theme = 'github';

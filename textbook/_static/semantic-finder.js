13ea41009162f7e1f8bd64f562a598c7f9900a23
Jiahan William Wen <wenjiahan2002@gmail.com>
2024-06-12 11:35:24 -0400

    change the customized html to another search bar

--- textbook/_static/semantic-finder.js
+++ textbook/_static/semantic-finder.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var searchField = document.querySelector(".bd-search");
-    var customNavContent = document.getElementById("custom-nav-content");
-    if (searchField && customNavContent) {
-      searchField.parentNode.insertBefore(customNavContent, searchField.nextSibling);
-    }
-  });
-  
+  // Select the default search form
+  var defaultSearchField = document.querySelector(".bd-search");
+  // Select the custom search form
+  var semanticSearchForm = document.getElementById("semantic-search-form");
+  // Check if both elements exist
+  if (defaultSearchField && semanticSearchForm) {
+    // Move the custom search form after the default search form
+    defaultSearchField.parentNode.insertBefore(semanticSearchForm, defaultSearchField.nextSibling);
+  }
+});
+

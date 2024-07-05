9b04322d85db6afbf83e9b437fb390d3046b528f
Jiahan William Wen <wenjiahan2002@gmail.com>
2024-07-05 09:20:08 -0400

    Overide search-field.html in Sphinx theme to include my custom search bar natively generated below the default one during build process, no more javascript/css is needed at all because the custom search bar is naturally exact same  with the default one excepr for this action="{{ pathto('semantic-search') }}" and a few id

--- textbook/_static/semantic-finder.js
+++ textbook/_static/semantic-finder.js
@@ -1,38 +1,45 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Select the default search form
-  var defaultSearchField = document.querySelector(".bd-search");
-  // Select the custom search form
-  var semanticSearchForm = document.getElementById("semantic-search-form");
-  // Check if both elements exist
-  if (defaultSearchField && semanticSearchForm) {
-    // Move the custom search form after the default search form
-    defaultSearchField.parentNode.insertBefore(semanticSearchForm, defaultSearchField.nextSibling);
-  }
+  // No Longer Needed as the custom search bar is integrated natively during build process
+  // through overriding semantic-field.html  at .venv/lib/python3.8/site-packages/pydata_sphinx_theme/theme/pydata_sphinx_theme/search-field.html
+  // by adding _templates/search_field.html and modifying textbook/_config.yml
 
-  // Separate Deployment and Local for correct relative paths
-  // Jumps to result page at search
-  const form = document.getElementById('semantic-search-form');
+// document.addEventListener("DOMContentLoaded", function() {
+//     // position custom Search Bar under default Search Bar in the left side Nav Bar
   
-  // Function to check if the environment is local
-  function isLocalEnvironment() {
-    return window.location.protocol === 'file:';
-  }
+//   // Select the default search form
+//   var defaultSearchField = document.querySelector(".bd-search");
+//   // Select the custom search form
+//   var semanticSearchForm = document.getElementById("semantic-search-form");
+//   // Check if both elements exist
+//   if (defaultSearchField && semanticSearchForm) {
+//     // Move the custom search form after the default search form
+//     defaultSearchField.parentNode.insertBefore(semanticSearchForm, defaultSearchField.nextSibling);
+//   }
 
-  // Set the form action based on the environment
-  if (isLocalEnvironment()) {
-    // For local environment
-    const pathArray = window.location.pathname.split('/');
-    const buildIndex = pathArray.indexOf('_build');
-    if (buildIndex !== -1) {
-      const localPath = pathArray.slice(0, buildIndex + 2).join('/');
-      form.action = `${localPath}/semantic-search.html`;
-    }
-  } else {
-    // For deployment environment
-    form.action = `${window.location.origin}/semantic-search.html`;
-  }
+//     // Separate Deployment and Local for correct relative paths
+//   // Jumps to result page at search
+//   const form = document.getElementById('semantic-search-form');
+  
+//   // Listen to Custome Search Bar
 
-});
+//   // Function to check if the environment is local
+//   function isLocalEnvironment() {
+//     return window.location.protocol === 'file:';
+//   }
+
+//   // Set the form action based on the environment
+//   if (isLocalEnvironment()) {
+//     // For local environment
+//     const pathArray = window.location.pathname.split('/');
+//     const buildIndex = pathArray.indexOf('_build');
+//     if (buildIndex !== -1) {
+//       const localPath = pathArray.slice(0, buildIndex + 2).join('/');
+//       form.action = `${localPath}/semantic-search.html`;
+//     }
+//   } else {
+//     // For deployment environment
+//     form.action = `${window.location.origin}/semantic-search.html`;
+//   }
+// });
 
 document.addEventListener("DOMContentLoaded", function() {
   const urlParams = new URLSearchParams(window.location.search);

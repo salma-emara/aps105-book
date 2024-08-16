04e9b2f70424ef759428279ab424f5941e02c9e7
rexhanh <rexhanh123@gmail.com>
2024-08-16 16:18:07 -0400

    chatbot first commit

---
+++ textbook/_static/chatbot.js
@@ -0,0 +1,101 @@
+document.write(`<head>
+  <!-- Font Awesome -->
+  <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+    rel="stylesheet"
+  />
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    rel="stylesheet"
+  />
+  <!-- MDB -->
+  <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.3.2/mdb.min.css"
+    rel="stylesheet"
+  />
+  <link rel="stylesheet" href="chatbot_styles.css" />
+  <script src="chatbot_actions.js"></script>
+
+  <script type="module" src="https://md-block.verou.me/md-block.js"></script>
+</head>
+
+<body>
+  <script
+    type="text/javascript"
+    src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.3.2/mdb.umd.min.js"
+  ></script>
+  <section>
+    <div class="float-chat-container hide" id="chat-box" style="height: auto">
+      <div class="row d-flex">
+        <div class="col-md-8 col-lg-6 col-xl-4" style="width: 100%">
+          <div class="card">
+            <div
+              class="card-header d-flex justify-content-between align-items-center"
+            >
+              <h5 class="mb-0">Bot</h5>
+              <div class="d-flex flex-row align-items-center">
+                <button
+                  class="btn-minus"
+                  title="Clear Chat"
+                  onclick="clearChat()"
+                >
+                  <i class="fa fa-trash"></i>
+                </button>
+                <button
+                  class="btn-minus"
+                  title="Minimize Chat"
+                  onclick="hideChat()"
+                >
+                  <i class="fa fa-minus"></i>
+                </button>
+              </div>
+            </div>
+            <div
+              class="card-body me-0"
+              data-mdb-perfect-scrollbar-init
+              style="position: relative"
+              id="chat"
+            ></div>
+            <div
+              class="card-footer text-muted d-flex justify-content-start align-items-center p-3"
+            >
+              <div class="input-group mb-0">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Type Your Quesstion Here"
+                  aria-label="Recipient's username"
+                  aria-describedby="button-addon2"
+                  id="userInput"
+                />
+                <button
+                  data-mdb-button-init
+                  data-mdb-ripple-init
+                  class="btn btn-primary"
+                  type="button"
+                  id="btn-submit"
+                  style="padding-top: 0.55rem"
+                  onclick="submit()"
+                >
+                  <i class="fas fa-paper-plane"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="float-btn-container">
+      <button
+        type="button"
+        class="btn btn-primary btn-floating btn-lg"
+        data-mdb-ripple-init
+        onclick="showChat()"
+      >
+        <i class="fas fa-plus"></i>
+      </button>
+    </div>
+  </section>
+</body>
+`);
---
+++ textbook/_static/chatbot_actions.js
@@ -0,0 +1,331 @@
+var is_followup = false;
+var is_pre_chat_rendered = false;
+if (sessionStorage.getItem("userData") === null) {
+  sessionStorage.setItem(
+    "userData",
+    JSON.stringify({
+      contexts: [],
+      latest_new_question: "",
+      messages: [],
+      sessionID: generateUUID(),
+    })
+  );
+}
+// Util Functions
+function generateUUID() {
+  var d = new Date().getTime(); //Timestamp
+  var d2 =
+    (typeof performance !== "undefined" &&
+      performance.now &&
+      performance.now() * 1000) ||
+    0; //Time in microseconds since page-load or 0 if unsupported
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16; //random number between 0 and 16
+    if (d > 0) {
+      //Use timestamp until depleted
+      r = (d + r) % 16 | 0;
+      d = Math.floor(d / 16);
+    } else {
+      //Use microseconds since page-load if supported
+      r = (d2 + r) % 16 | 0;
+      d2 = Math.floor(d2 / 16);
+    }
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+  });
+}
+
+function updateUserDataInSessionStorage(
+  messages,
+  contexts,
+  latest_new_question,
+  sessionID
+) {
+  sessionStorage.setItem(
+    "userData",
+    JSON.stringify({ contexts, latest_new_question, messages, sessionID })
+  );
+}
+
+function wait(delay) {
+  return new Promise((resolve) => setTimeout(resolve, delay));
+}
+
+// Chat Bot Functions
+// Render the saved messages
+function renderPrevChats() {
+  if (!is_pre_chat_rendered) {
+    var curr_userData = JSON.parse(sessionStorage.getItem("userData"));
+    var messages = curr_userData["messages"];
+    for (let i = 0; i < messages.length; i++) {
+      if (messages[i]["role"] === "user") {
+        createUserMessage(messages[i]["content"]);
+      } else if (messages[i]["role"] === "assistant") {
+        createBotMessage(messages[i]["content"], true);
+      }
+      if (i == messages.length - 1 && messages[i]["role"] == "assistant") {
+        createChooseButtons();
+      }
+    }
+    is_pre_chat_rendered = true;
+  }
+}
+function pushMessage(message, role) {
+  // Push the message to sessionStorage
+  const userData = JSON.parse(sessionStorage.getItem("userData"));
+  var messages = userData["messages"];
+  messages.push({ role: role, content: message });
+  updateUserDataInSessionStorage(
+    messages,
+    userData["contexts"],
+    userData["latest_new_question"],
+    userData["sessionID"]
+  );
+}
+
+function submit() {
+  // Get input element
+  var input = document.getElementById("userInput");
+  if (input.value === "") {
+    return;
+  }
+  const user_query = input.value;
+
+  // Reset Input
+  input.value = "";
+  input.disabled = true;
+  // Create user message
+  createUserMessage(user_query);
+
+  // Push user message
+  pushMessage(user_query, "user");
+
+  // Update the latest question if is not a follow up
+  if (!is_followup) {
+    const userData = JSON.parse(sessionStorage.getItem("userData"));
+    updateUserDataInSessionStorage(
+      userData["messages"],
+      userData["contexts"],
+      user_query,
+      userData["sessionID"]
+    );
+  }
+
+  // Fetch message
+  getResponse();
+}
+
+function getResponse() {
+  createLoading();
+  scrollToBottom();
+  const url = "http://aps105.ece.utoronto.ca:8090/api/v1/chat";
+  const userData = JSON.parse(sessionStorage.getItem("userData"));
+  fetchRetry(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ userData, message_is_followup: is_followup }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      removeLoading();
+      const messages = data.userData.messages;
+      const contexts = data.userData.contexts;
+      const latest_new_question = data.userData.latest_new_question;
+      const new_message = messages[messages.length - 1].content;
+      createBotMessage(new_message, true);
+      createChooseButtons();
+      // update userData
+      sessionStorage.setItem(
+        "userData",
+        JSON.stringify({
+          contexts: contexts,
+          latest_new_question: latest_new_question,
+          messages: messages,
+          sessionID: data.userData.sessionID,
+        })
+      );
+    })
+    .then(() => {
+      scrollToBottom();
+    })
+    .catch((error) => {
+      removeLoading();
+      createBotMessage("Something went wrong ...", false);
+      scrollToBottom();
+    });
+}
+
+function fetchRetry(url, options, retries = 3) {
+  return fetch(url, options)
+    .then((res) => {
+      if (res.ok) {
+        return res;
+      }
+      if (retries > 0) {
+        return wait(1000).then(() => fetchRetry(url, options, retries - 1));
+      } else {
+        throw new Error(res);
+      }
+    })
+    .catch(console.error);
+}
+
+// Chat Front End
+function createUserMessage(message) {
+  const newDiv = document.createElement("div");
+  newDiv.className = "d-flex flex-row justify-content-end mt-0";
+  newDiv.id = "user-message";
+  newDiv.style = "min-height: auto;";
+  const newDiv1 = document.createElement("div");
+  const paragraph = document.createElement("p");
+  paragraph.textContent = message;
+  paragraph.className = "small p-2 me-0 text-white rounded-3 bg-primary";
+  newDiv1.appendChild(paragraph);
+  newDiv.appendChild(newDiv1);
+
+  const parent = document.getElementById("chat");
+  parent.appendChild(newDiv);
+}
+
+function createBotMessage(message, is_markdown) {
+  var message = message.replaceAll("<", "&lt");
+  const newDiv = document.createElement("div");
+  newDiv.className = "d-flex flex-row justify-content-start mt-0";
+  newDiv.id = "bot-message";
+  if (is_markdown === false) {
+    newDiv.style = "height: auto; width: 50%;";
+  } else {
+    newDiv.style = "height: auto;";
+  }
+  const newDiv1 = document.createElement("div");
+  newDiv1.style = "width: 100%;";
+  if (!is_markdown) {
+    const paragraph = document.createElement("p");
+    paragraph.textContent = message;
+    paragraph.className = "small p-2 me-0 mb-5 rounded-3 bg-body-tertiary";
+    newDiv1.appendChild(paragraph);
+  } else {
+    const html = `<md-block>${message}</md-block>`;
+    newDiv1.insertAdjacentHTML("beforeend", html);
+  }
+
+  newDiv.appendChild(newDiv1);
+
+  const parent = document.getElementById("chat");
+  parent.appendChild(newDiv);
+}
+
+function createChooseButtons() {
+  // Create New Question and Follow Up question
+  const newDiv = document.createElement("div");
+  newDiv.className = "d-flex flex-row justify-content-start mt-1";
+  newDiv.id = "bot-message-choose";
+  newDiv.style = "height: auto;";
+  const newDiv1 = document.createElement("div");
+  newDiv1.className = "d-flex flex-row";
+  newDiv1.style = "gap: 20px";
+
+  const btn0 = document.createElement("button");
+  btn0.className = "btn btn-primary";
+  btn0.textContent = "followup";
+  btn0.id = "followup-question";
+  btn0.onclick = function () {
+    handleChooseButtonClick("followup-question");
+  };
+
+  const btn1 = document.createElement("button");
+  btn1.className = "btn btn-primary";
+  btn1.textContent = "New Question";
+  btn1.id = "new-question";
+  btn1.onclick = function () {
+    handleChooseButtonClick("new-question");
+  };
+
+  newDiv1.appendChild(btn0);
+  newDiv1.appendChild(btn1);
+  newDiv.appendChild(newDiv1);
+
+  const parent = document.getElementById("chat");
+  parent.appendChild(newDiv);
+
+  const input = document.getElementById("userInput");
+  input.disabled = true;
+}
+
+function handleChooseButtonClick(buttonID) {
+  // Handle the click of followup or new question
+  const input = document.getElementById("userInput");
+  input.disabled = false;
+
+  const buttons = document.getElementById("bot-message-choose");
+  buttons.remove();
+  if (buttonID === "followup-question") {
+    is_followup = true;
+  } else {
+    is_followup = false;
+  }
+}
+
+function createLoading() {
+  // Create loading animation
+  const div = document.createElement("div");
+  div.className = "loader";
+  div.id = "loading";
+  const parent = document.getElementById("chat");
+  parent.appendChild(div);
+}
+
+function removeLoading() {
+  // Remove loading animation
+  const loadingDiv = document.getElementById("loading");
+  loadingDiv.remove();
+}
+
+function scrollToBottom() {
+  // Scroll to the bottom of the chat
+  const parent = document.getElementById("chat");
+  parent.scrollTop = parent.scrollHeight;
+}
+
+// Actions
+function showChat() {
+  const chatBox = document.getElementById("chat-box");
+  chatBox.classList.remove("hide");
+  chatBox.classList.add("show");
+  var input = document.getElementById("userInput");
+  // Add Enter key to submit
+  input.addEventListener("keypress", function (event) {
+    if (event.key == "Enter") {
+      event.preventDefault();
+      document.getElementById("btn-submit").click();
+    }
+  });
+  renderPrevChats();
+}
+
+function clearChat() {
+  // Deletes the messages and hide the chat
+  const chats = document.getElementById("chat");
+  chats.innerHTML = "";
+  // Input is enabled
+  const input = document.getElementById("userInput");
+  input.disabled = false;
+  // Reset Chat history
+  const sessionID = JSON.parse(sessionStorage.getItem("userData"))["sessionID"];
+  updateUserDataInSessionStorage([], [], "", sessionID);
+}
+
+function closeChat() {
+  // Clear the chatbox
+  clearChat();
+  // Hide the chatbox
+  hideChat();
+}
+
+function hideChat() {
+  // Hides the chat only
+  const chatBox = document.getElementById("chat-box");
+  // chatBox.style.display = "none";
+  chatBox.classList.add("hide");
+}

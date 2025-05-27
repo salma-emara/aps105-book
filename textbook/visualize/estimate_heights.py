3ca5785e26148fac98ab5aa9d494d698e28373f4
JinningL <imjinning.liu@mail.utoronto.ca>
2025-05-27 11:14:26 -0400

    Delete old version

--- textbook/visualize/estimate_heights.py
+++
@@ -1,9 +0,0 @@
-
-def get_estimated_height(code):
-    line_count = code.count('\n') + 1
-    base_height = 300   
-    per_line_height = 15
-    max_height = 700     
-
-    height = base_height + per_line_height * line_count
-    return min(height, max_height)
--- textbook/visualize/examples/chapter3/test3.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-    int a=5,b=10;
-    int c=a+b;
-    printf("%d",c);
-    return 0;
-}

5ede2780a1cb9936f29c547a88c73abbc779d3e1
Nora Liu <noraliu510@gmail.com>
2025-08-19 02:10:26 -0400

    Chapter 8 and 10

---
+++ textbook/exercises/chapter-8/testing-exercises.js
@@ -0,0 +1,23 @@
+let parsedObject; 
+  parsedObject = {
+  "exercises": [
+    {
+      "title": "Question 4 in Winter 2018 Final Exam",
+      "difficulty": "Easy",
+      "type": "explaination",
+      "table": false,
+      "multipart": false,
+      "question": "Write a single C statement that declares a variable called `intPtrArray`, initialized to point to an array of 10 integers that is dynamically allocated.\n",
+      "answer": " \nint *intPtrArray = (int *)malloc(10 * sizeof(int));\n"
+    },
+    {
+      "title": "Question 2 in Fall 2015 Final Exam",
+      "difficulty": "Easy",
+      "type": "explaination",
+      "table": false,
+      "multipart": false,
+      "question": "Write one or more C statements that use `malloc` to dynamically allocate an array of $1000$ elements of type `double`. The allocated array should be called `list`.\n",
+      "answer": " \ndouble *list = (double *)malloc(1000 * sizeof(double));\n"
+    }
+  ]
+};

4e780158647e30f4d7f7a2bb767f7821169c4655
Nora Liu <noral.liu@mail.utoronto.ca>
2026-04-03 01:28:45 -0400

    Reapply "Updated question placeholder"
    This reverts commit bd7da6b0457544e7495ce5e291a2a0ea301fd8f8.

--- textbook/trace/exercises/example1/testing-exercises.js
+++ textbook/trace/exercises/example1/testing-exercises.js
@@ -8,9 +8,8 @@ let parsedObject;
       "table": false,
       "multipart": false,
       "type": "visualizer",
-      "question": "`This text is pink`\n**This text is bold** \n\nTrace the following code:\n",
-      "starter-code": "#include <stdio.h>\n\nint main(void) {\n\n  printf(\"Hello World!\\n\");\n\n  return 0;\n}\n",
-      "answer": "Hello World!\n"
+      "question": "`This text is pink`\n**This text is bold** \n\n```{code-block} c\n#include <stdio.h>\n\nint main(void) {\n\n  printf(\"This is exercise 1!\\n\");\n\n  return 0;\n}\n```\n",
+      "answer": "This is exercise 1!\n"
     }
   ]
 };
